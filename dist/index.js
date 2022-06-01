fe17d21f8844d2e4fb40bfb8e263f786fb2816a2
GrayAi0 <brhoom234567@gmail.com>
2022-06-01 03:14:23 +0300

    UPDATE: 0.1.1

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,8 @@
 
 'use strict'
+
 if (process.env.NODE_ENV === 'production') {
-  console.log("Production")
   module.exports = require('./console-menu.js.cjs.production.min.js')
 } else {
-  console.log("development")
   module.exports = require('./console-menu.js.cjs.development.js')
 }
